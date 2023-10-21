222c856669bbb9e4c9396b5a1454d00eb520fbff
vaibhav sukale <104653559+itsvaibhavcoder@users.noreply.github.com>
2023-10-22 00:14:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,3 +3,39 @@
 // what is compenent ?
 // React apps are made out of components. A component is a piece of the UI (user interface) that has its own logic and appearance. A component can be as small as a button, or as large as an entire page.
 
+// React components are JavaScript functions that return markup
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
+}
+// Now that you’ve declared MyButton, you can nest it into another component
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  );
+}
+// Notice that <MyButton /> starts with a capital letter. That’s how you know it’s a React component. React component names must always start with a capital letter, while HTML tags must be lowercase.
+// Now Our App.js code will be 
+
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
+  );
+}
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  );
+}
+
+
+
