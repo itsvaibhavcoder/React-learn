79c1a33af19ad4f517d9e6c8afa8f30b1f4becf6
vaibhav sukale <104653559+itsvaibhavcoder@users.noreply.github.com>
2023-10-22 00:08:34 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+// 1) Creating and nesting components
+
+// what is compenent ?
+// React apps are made out of components. A component is a piece of the UI (user interface) that has its own logic and appearance. A component can be as small as a button, or as large as an entire page.
+
