21db2b9cb548f1655a4071b7e9e75afc0c2cb603
vaibhav sukale <104653559+itsvaibhavcoder@users.noreply.github.com>
2023-10-22 16:17:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -72,4 +72,40 @@ return (
   </h1>
 );
 
+// You can also “escape into JavaScript” from JSX attributes, but you have to use curly braces instead of quotes. For example, className="avatar" passes the "avatar" string as the CSS class, but src={user.imageUrl} reads the JavaScript user.imageUrl variable value, and then passes that value as the src attribute:
+
+return (
+  <img
+    className="avatar"
+    src={user.imageUrl}
+  />
+);
+
+// Overall app,js looks like
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+export default function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  );
+}
+
+//In the above example, style={{}} is not a special syntax, but a regular {} object inside the style={ } JSX curly braces. You can use the style attribute when your styles depend on JavaScript variables
+
+
 
