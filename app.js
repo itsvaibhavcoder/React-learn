8236df3e66e3e78322db2d342732f522d4e18c79
vaibhav sukale <104653559+itsvaibhavcoder@users.noreply.github.com>
2023-10-22 16:31:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -107,5 +107,35 @@ export default function Profile() {
 
 //In the above example, style={{}} is not a special syntax, but a regular {} object inside the style={ } JSX curly braces. You can use the style attribute when your styles depend on JavaScript variables
 
+//5) Conditional rendering 
+//In React, there is no special syntax for writing conditions. Instead, you’ll use the same techniques as you use when writing regular JavaScript code. For example, you can use an if statement to conditionally include JSX
+
+let content;
+if (isLoggedIn) {
+  content = <AdminPanel />;
+} else {
+  content = <LoginForm />;
+}
+return (
+  <div>
+    {content}
+  </div>
+);
+
+// If you prefer more compact code, you can use the conditional ? operator. Unlike if, it works inside JSX
+<div>
+  {isLoggedIn ? (
+    <AdminPanel />
+  ) : (
+    <LoginForm />
+  )}
+</div>
+
+//When you don’t need the else branch, you can also use a shorter logical && syntax
+<div>
+  {isLoggedIn && <AdminPanel />}
+</div>
+
+
 
 
