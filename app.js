086aa57d221c5885a8d88b89a46712a0a7b442ef
vaibhav sukale <104653559+itsvaibhavcoder@users.noreply.github.com>
2023-10-22 15:59:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -38,10 +38,38 @@ export default function MyApp() {
 }
 
 //2)Writing markup with JSX 
-//The markup syntax you’ve seen above is called JSX.
 
+//The markup syntax you’ve seen above is called JSX.
 //JSX is stricter than HTML. You have to close tags like <br />. Your component also can’t return multiple JSX tags. You have to wrap them into a shared parent, //like a <div>...</div> or an empty <>...</> wrapper
 
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>Hello there.<br />How do you do?</p>
+    </>
+  );
+}
 
+
+//3)Adding styles
+
+//In React, you specify a CSS class with className. It works the same way as the HTML class attribute
+
+<img className="avatar" />
+/* In your CSS */
+.avatar {
+  border-radius: 50%;
+}
+
+//4)Displaying data
+
+//JSX lets you put markup into JavaScript. Curly braces let you “escape back” into JavaScript so that you can embed some variable from your code and display it to //the user
+
+return (
+  <h1>
+    {user.name}
+  </h1>
+);
 
 
